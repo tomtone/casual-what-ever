829d703dcec42af0558b3da0bd3827246ec7510b
Thomas <thomas.v.gostomski@gmx.de>
2013-04-07 19:33:44 +0200

    added general db connection params

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -11,6 +11,37 @@
  * file.
  */
 
+$dbParams = array(
+    'database'  => 'browsergame',
+    'username'  => 'root',
+    'password'  => '',
+    'hostname'  => 'localhost',
+    // buffer_results - only for mysqli buffered queries, skip for others
+    'options' => array('buffer_results' => true)
+);
+
 return array(
-    // ...
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter' => function ($sm) use ($dbParams) {
+                $adapter = new BjyProfiler\Db\Adapter\ProfilingAdapter(array(
+                    'driver'    => 'pdo',
+                    'dsn'       => 'mysql:dbname='.$dbParams['database'].';host='.$dbParams['hostname'],
+                    'database'  => $dbParams['database'],
+                    'username'  => $dbParams['username'],
+                    'password'  => $dbParams['password'],
+                    'hostname'  => $dbParams['hostname'],
+                ));
+
+                $adapter->setProfiler(new BjyProfiler\Db\Profiler\Profiler);
+                if (isset($dbParams['options']) && is_array($dbParams['options'])) {
+                    $options = $dbParams['options'];
+                } else {
+                    $options = array();
+                }
+                $adapter->injectProfilingStatementPrototype($options);
+                return $adapter;
+            },
+        ),
+    ),
 );
