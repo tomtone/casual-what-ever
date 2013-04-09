e869081357bd1d4f29b77b1b8ab5e19814e2e019
mbunge <marco.bunge@rubymatrix.de>
2013-04-10 00:27:27 +0200

    update schema

--- module/Gameunit/data/schema.sql
+++ module/Gameunit/data/schema.sql
@@ -7,17 +7,16 @@ CREATE TABLE IF NOT EXISTS `gameunit_unit` (
   KEY `name` (`name`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin ;
 
-CREATE TABLE IF NOT EXISTS `gameunit_stats` (
+CREATE TABLE IF NOT EXISTS `gameunit_stats_relation` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `unit_id` int(11) NOT NULL DEFAULT '0',
-  `type_id` varchar(255) COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `name` (`name`)
+  `unit_id` int(11) NOT NULL,
+  `stat_type_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin ;
 
 CREATE TABLE IF NOT EXISTS `gameunit_stat_types` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL DEFAULT '0',
+  `name` varchar(255) COLLATE utf8_bin NOT NULL,
   `value` varchar(255) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`),
   KEY `name` (`name`)
